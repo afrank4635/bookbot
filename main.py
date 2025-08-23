aa0917c456241db2c06d210410b426711c8542fe
afrank4635 <andrew.tc.frank@gmail.com>
2025-08-24 00:01:13 +0900

    added option to choose text to analyze from directory

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import sys
 from stats import get_num_words, sort_on, get_num_chars, sort_chars
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path) 
     num_words = get_num_words(text)
     chars_dict = get_num_chars(text)
     chars_sort = sort_chars(chars_dict)
-
-
-
 
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
@@ -28,5 +26,8 @@ def get_book_text(book_name):
         return file.read()
 
 
-
-main()
+if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+else:
+    main()
